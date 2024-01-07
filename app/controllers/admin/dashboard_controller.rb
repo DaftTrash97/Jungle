7fd80b67ba8c5c3a969e14d6acebed673fe1ec3e
Steph Nugs <steph.bowie1993@gmail.com>
2024-01-06 19:25:43 -0700

    update username

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,8 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin
-
   def show
-    # Your existing code for the show action
   end
-
-  private
-
+  
   def authenticate_admin
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
