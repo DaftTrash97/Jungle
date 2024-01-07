b5a9a7e72044f11a358468b595460e23445d26cc
Steph Nugs <steph.bowie1993@gmail.com>
2024-01-06 19:35:23 -0700

    create before action load_stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,17 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin
+  before_action :load_stats, only: [:show]
   def show
   end
-  
+
   def authenticate_admin
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
     end
+  end
+
+  def load_stats
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
